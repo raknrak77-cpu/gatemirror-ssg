a753809422d478065c0c620f49a6b0d7f024dd49
raknrak77-cpu <raknrak77@gmail.com>
2026-04-20 01:39:57 +0300

    Update uploader.py

--- bots/uploader.py
+++ bots/uploader.py
@@ -73,7 +73,7 @@ def upload_svg_patterns():
     print("-" * 40)
     
     svg_files = [
-        ("assets/all-patterns/spiral_out/spiral_circular_basic_16.svg", "assets/svg1.svg"),
+        ("assets/all-patterns/spiral_out/spiral_circular_basic_07.svg", "assets/svg1.svg"),
         ("assets/all-patterns/spiral_out/spiral_circular_basic_20.svg", "assets/svg2.svg"),
     ]
     
